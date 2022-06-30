0c72c255093067c545228d20e14e03ae82ba784b
Lucas Jahier <lucas.jahier@jahier.dev>
2022-06-30 02:09:03 +0200

    chore: remove useless code

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,49 +1,40 @@
 //Lib
 import React from "react";
-import classes from './Task.module.css';
+import classes from "./Task.module.css";
 
 //PrimeReact CSS
-import { Checkbox } from 'primereact/checkbox';
-import { Button } from 'primereact/button'
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";                                //icons
+import { Checkbox } from "primereact/checkbox";
+import { Button } from "primereact/button";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import "primereact/resources/primereact.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
 
 function Task(props) {
-    
-    //// Methodes
-    // Affichage d'une case cochée ou non + texte barré
-    const isCheck = check => {
-        if (check) {
-            return (
-                <div className="field-checkbox">
-                    <Checkbox inputId={props.index} checked={props.check} onChange={props.checkTask}/>
-                    <div className={classes.taskName}>
-                        <strike>{props.taskName}</strike>
-                    </div>
-                </div>         
-            );
-        }
-        else {
-            return (
-                <div className="field-checkbox">
-                    <Checkbox inputId={props.index} checked={props.check} onChange={props.checkTask}/>
-                    <div className={classes.taskName}>
-                        {props.taskName}
-                    </div>
-                </div>  
-            );
-        }
-    }
-    
-    return (
-      <div className={classes.task}>
-        
-        {isCheck(props.check)}
-            
-        <Button icon="pi pi-times" className="p-button-rounded p-button-danger p-button-text p-button-sm" aria-label="Cancel" onClick={props.deleteTask} />
+  return (
+    <div className={classes.task}>
+      <div className="field-checkbox">
+        <Checkbox
+          inputId={props.index}
+          checked={props.check}
+          onChange={props.checkTask}
+        />
+        <div className={classes.taskName}>
+          {props.check ? (
+            <strike>{props.taskName}</strike>
+          ) : (
+            <>{props.taskName}</>
+          )}
+        </div>
       </div>
-    );
+
+      <Button
+        icon="pi pi-times"
+        className="p-button-rounded p-button-danger p-button-text p-button-sm"
+        aria-label="Cancel"
+        onClick={props.deleteTask}
+      />
+    </div>
+  );
 }
 
 export default Task;
